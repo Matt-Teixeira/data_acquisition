ad6357f8861615bb6186fc5919cc24d7a3c3430c
Matt Teixeira <matt-teixeira@vm-debian.c.oceanic-citadel-185620.internal>
2024-04-04 12:12:30 +0000

    Updated configs

--- db/normalize/staging/siemens_ct_norm.sql
+++ db/normalize/staging/siemens_ct_norm.sql
@@ -1072,3 +1072,33 @@ VALUES(
 	NULL,
 	NULL
 );
+
+-->
+-->
+
+INSERT INTO config.acquisition(system_id, host_ip, mmb_ip, protocal, debian_server_path, credentials_group, acquisition_script, run_group, host, user_id, acqu_point, file_version)
+VALUES (
+	'SME01832',
+	'10.30.0.75',
+	NULL,
+	'lftp',
+	'/home/staging/hhm_data_acquisition/files/SME01832',
+	NULL,
+	'siemens_80_data_grab.sh',
+	1,
+	NULL,
+	NULL,
+	NULL,
+	'win_10'
+);
+
+INSERT INTO config.log (system_id, file_name, dir_name, regex_models, pg_tables, column_name, agg)
+VALUES(
+	'SME01832',
+	'Application.log',
+	'Application',
+	ARRAY['re_v1'], -- regex
+	ARRAY['siemens_ct'], -- tables
+	NULL,
+	NULL
+);
