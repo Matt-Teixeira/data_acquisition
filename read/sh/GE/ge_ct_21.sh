049f7fd49bbe88eae11934b41fa8f39532d33b72
Matt Teixeira <matt-teixeira@vm-debian.c.oceanic-citadel-185620.internal>
2025-07-31 19:01:43 +0000

    refactor to reduce time to connection error

--- read/sh/GE/ge_ct_21.sh
+++ read/sh/GE/ge_ct_21.sh
@@ -1,5 +1,4 @@
-[ ! -d "$4" ] && mkdir $4
-timeout 130 lftp ftp://$2:$3@$1 -e "set net:reconnect-interval-base 5; set net:max-retries 2; mirror --only-newer --include='gesys*' --exclude ".\+/$" --newer-than=now-7days /usr/g/service/log/ $4"
+#!/bin/bash
 
-## lftp -c "open ftp://$2:$3@$1; cd /C/Program\ Files/GE\ Medical\ Systems/DL/Log/; mget -e sysError.log -O $4/sysError"
-## vs ftp
+[ ! -d "$4" ] && mkdir $4
+timeout 55 lftp ftp://$2:$3@$1 -e "set net:reconnect-interval-base 5; set net:max-retries 2; mirror --only-newer --include='gesys*' --exclude ".\+/$" --newer-than=now-7days /usr/g/service/log/ $4"
