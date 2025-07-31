57860d02905f2438d2574524b611ec6c0c9fa477
Matt Teixeira <matt-teixeira@vm-debian.c.oceanic-citadel-185620.internal>
2025-07-31 18:15:12 +0000

    notes for now - delete later

--- sql/system/get_hhm_data.sql
+++ sql/system/get_hhm_data.sql
@@ -17,3 +17,16 @@ WHERE
     manufacturer = $1
     AND modality LIKE $2
     AND process_log = true;
+
+--> GE_CT
+-- Connecting              SME00897
+-- Timed Out               SME00847
+-- Network Hang Code: 124  SME17366 SME17367
+--> 'SME17367', 'SME00897', 'SME00847', 'SME17366'
+-- SME01433 TEST FOR CRED SWITCH UP
+
+--> GE_CV
+-- Connecting              
+-- Timed Out               
+-- Network Hang Code: 124  
+--> 
