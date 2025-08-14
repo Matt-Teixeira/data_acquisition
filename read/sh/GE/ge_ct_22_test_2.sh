b769bc54068bf584e5c0a0460445943d4d92e1da
Matt Teixeira <matt-teixeira@vm-debian.c.oceanic-citadel-185620.internal>
2025-08-14 16:17:42 +0000

    Test to replace parellel connections with lftp

---
+++ read/sh/GE/ge_ct_22_test_2.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+[ ! -d "$4" ] && mkdir $4
+
+lftp -c "set sftp:connect-program 'ssh -o KexAlgorithms=diffie-hellman-group1-sha1,diffie-hellman-group-exchange-sha256,diffie-hellman-group14-sha1,diffie-hellman-group-exchange-sha1,diffie-hellman-group14-sha256'; 
+set net:timeout 10; 
+set ftp:ssl-allow off; 
+set net:reconnect-interval-base 5; 
+set net:max-retries 1; 
+set xfer:clobber true; 
+open sftp://$2:$3@$1; 
+cd /usr/g/service/log; 
+mget gesys*.log -O $4; 
+cd /usr/g/service/state;
+mget ssw.dastools.hist* -O $4; 
+mget ssw.calreport.hist -O $4;
+mget ssw.GenCal.hist -O $4;
+mget ssw.GenCal.hist -O $4;
+mget ssw.tube_align.hist -O $4;
+mget air_bubble_detection_result.csv $4;
+mget TubeSpits.csv $4;
+mget image_analysis.csv $4;
+mget ssw.srv_activity.ref $4;
+mget ssw.diagSession.hist $4;
+cd /var/log;
+mget messages* -O $4;"
