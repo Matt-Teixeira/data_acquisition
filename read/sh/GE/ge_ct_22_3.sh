fc3b725039798c2409c76ca4943e8f91a201b140
Matt Teixeira <matt-teixeira@vm-debian.c.oceanic-citadel-185620.internal>
2025-08-20 14:32:14 +0000

    Script to not pull messages files

---
+++ read/sh/GE/ge_ct_22_3.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+[ ! -d "$4" ] && mkdir $4
+
+lftp -c "set sftp:connect-program 'ssh -o KexAlgorithms=diffie-hellman-group1-sha1'; 
+set net:timeout 10; 
+set ftp:ssl-allow off; 
+set net:reconnect-interval-base 5; 
+set net:max-retries 1; 
+set xfer:clobber true; 
+open sftp://$2:$3@$1; 
+cd /usr/g/service/log; 
+mget gesys*.log -O $4; 
+cd /usr/g/service/state;
+mget ssw.dastools.hist* -O $4; 
+mget ssw.calreport.hist -O $4;
+mget ssw.GenCal.hist -O $4;
+mget ssw.GenCal.hist -O $4;
+mget ssw.tube_align.hist -O $4;
+mget air_bubble_detection_result.csv -O $4;
+mget TubeSpits.csv -O $4;
+mget image_analysis.csv -O $4;
+mget ssw.srv_activity.ref -O $4;
+mget ssw.diagSession.hist -O $4;"
