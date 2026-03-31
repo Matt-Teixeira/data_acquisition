faa8d1188469d5b573e13072c7c4458f4c7df17a
Matt Teixeira <matt-teixeira@vm-debian.c.oceanic-citadel-185620.internal>
2026-03-31 17:00:12 +0000

    Updated

--- db/normalize/prod/philips_ct_norm.sql
+++ db/normalize/prod/philips_ct_norm.sql
@@ -959,3 +959,43 @@ VALUES(
 	NULL,
 	NULL
 );
+
+-- > 3/6/26
+-- >
+
+INSERT INTO config.acquisition(system_id, host_ip, mmb_ip, protocal, debian_server_path, credentials_group, acquisition_script, run_group, host, user_id, acqu_point)
+VALUES (
+	'SME20611',
+	'10.201.45.23',
+	NULL,
+	NULL,
+	'/home/prod/hhm_data_acquisition/files/SME20611',
+	'13',
+	'phil_ct_data_grab_1.sh',
+	1,
+	NULL,
+	NULL,
+	NULL
+);
+
+INSERT INTO config.log (system_id, file_name, dir_name, regex_models, pg_tables, column_name, agg)
+VALUES(
+	'SME20611',
+	'Events.output',
+	'events',
+	ARRAY['ct_events_v2'], -- regex
+	ARRAY['philips_ct_events'], -- tables
+	NULL,
+	NULL
+);
+
+INSERT INTO config.log (system_id, file_name, dir_name, regex_models, pg_tables, column_name, agg)
+VALUES(
+	'SME20611',
+	'EALInfo.output',
+	'eal',
+	ARRAY['ct_eal_v_2'], -- regex
+	ARRAY['philips_ct_eal'], -- tables
+	NULL,
+	NULL
+);
--- db/normalize/prod/siemens_ct_norm.sql
+++ db/normalize/prod/siemens_ct_norm.sql
@@ -952,3 +952,33 @@ VALUES(
 	NULL,
 	NULL
 );
+
+--> 3-11-26
+-->
+
+INSERT INTO config.acquisition(system_id, host_ip, mmb_ip, protocal, debian_server_path, credentials_group, acquisition_script, run_group, host, user_id, acqu_point, file_version)
+VALUES (
+	'SME01427',
+	'172.20.231.246',
+	NULL,
+	'lftp',
+	'/home/prod/hhm_data_acquisition/files/SME01427',
+	NULL,
+	'siemens_443_data_grab.sh',
+	1,
+	NULL,
+	NULL,
+	NULL,
+	'win_10'
+);
+
+INSERT INTO config.log (system_id, file_name, dir_name, regex_models, pg_tables, column_name, agg)
+VALUES(
+	'SME01427',
+	'Application.log',
+	'Application',
+	ARRAY['re_v1'], -- regex
+	ARRAY['siemens_ct'], -- tables
+	NULL,
+	NULL
+);
